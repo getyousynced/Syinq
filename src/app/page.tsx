ec9a2e15b4568803cf3a6fd061051dd89a01b736
Aanshuu <e21cseu0085@bennett.edu.in>
2024-10-01 06:35:44 +0530

    Update package dependencies and add email subscription feature to footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
-import Image from "next/image";
+'use client'
+
 import Landing from "./components/Landing";
+import Footer from "./components/Footer";
 
 export default function Home() {
   return (
     <div>
       <Landing />
+      <Footer/>
     </div>
   );
 }
