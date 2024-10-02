012c61e35b674047e2d3a0244237633691c965d4
Aanshuu <e21cseu0085@bennett.edu.in>
2024-10-02 13:41:13 +0530

    refactor(ui): update footer layout

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -42,13 +42,13 @@ const Footer = () => {
       </div>
       <div className="relative z-10 max-w-7xl mx-auto px-4">
         <div className="flex flex-col items-center">
-          <div className="border-2 rounded-xl bg-white flex flex-col md:flex-row items-center justify-between px-8 py-16 gap-4 mt-16 shadow-xl w-full max-w-7xl mx-auto">
-            <h2 className="text-3xl w-full text-nowrap font-thin">
+          <div className="border-2 rounded-xl bg-white flex flex-col md:flex-row items-center justify-between px-4 md:px-8 py-8 md:py-16 gap-4 mt-8 md:mt-16 shadow-xl w-full max-w-7xl mx-auto">
+            <h2 className="text-xl md:text-3xl w-full text-nowrap font-thin">
               Subscribe Newsletters
             </h2>
             <form
               onSubmit={handleSubmit}
-              className="flex items-center space-x-4 max-w-2xl w-full"
+              className="flex flex-col md:flex-row items-center space-y-4 md:space-x-4 max-w-2xl w-full"
             >
               <div className="relative w-full max-w-lg">
                 {/* Input field */}
@@ -58,63 +58,63 @@ const Footer = () => {
                   onChange={(e) => setEmail(e.target.value)}
                   required
                   placeholder="Enter your email"
-                  className="w-full p-6 pr-72 text-lg border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+                  className="w-full p-4 md:p-6 pr-32 md:pr-60 text-sm md:text-lg border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
                 />
                 {/* Button inside the input field */}
                 <button
                   type="submit"
-                  className="absolute right-2 top-1/2 transform -translate-y-1/2 bg-blue-500 text-white px-8 py-4 rounded-md hover:bg-blue-600 transition duration-300"
+                  className="text-sm md:text-xl absolute right-2 top-1/2 transform -translate-y-1/2 bg-blue-500 text-white px-2 md:px-8 py-2 md:py-4 rounded-md hover:bg-blue-600 transition duration-300"
                 >
                   Subscribe Now
                 </button>
               </div>
             </form>
           </div>
-          <div className="w-full py-12">
+          <div className="w-full py-6 md:py-12">
             <div className="container mx-auto flex flex-col md:flex-row items-center justify-between space-y-4 md:space-y-0">
-              
-              <div className="flex justify-center space-x-20 text-[#434341]">
-                <a href="#" className="hover:text-black text-lg">
+              <div className="flex justify-center space-x-10 md:space-x-20 text-[#434341]">
+                <a href="#" className="hover:text-black text-sm text-nowrap md:text-lg">
                   About us
                 </a>
-                <a href="#" className="hover:text-black text-lg">
+                <a href="#" className="hover:text-black text-sm text-nowrap md:text-lg">
                   Contact us
                 </a>
-                <a href="#" className="hover:text-black text-lg">
+                <a href="#" className="hover:text-black text-sm text-nowrap md:text-lg">
                   About
                 </a>
-                <a href="#" className="hover:text-black text-lg">
+                <a href="#" className="hover:text-black text-sm text-nowrap md:text-lg">
                   Blogs
                 </a>
               </div>
 
-              <div className="flex justify-center space-x-8 text-gray-500">
-                <a href="#" className="text-[#434341] hover:text-black mt-0.5">
-                  <ImFacebook2  size={32} />
+              <div className="flex justify-center space-x-6 md:space-x-8 text-gray-500">
+                <a href="#" className="text-[#434341] hover:text-black">
+                  <ImFacebook2 size={28} />
                 </a>
                 <a href="#" className="text-[#434341] hover:text-black">
-                  <FaSquareXTwitter size={36} />
+                  <FaSquareXTwitter size={32} />
                 </a>
                 <a href="#" className="text-[#434341] hover:text-black">
-                  <FaSquareVimeo size={36} />
+                  <FaSquareVimeo size={32} />
                 </a>
                 <a href="#" className="text-[#434341] hover:text-black">
-                  <FaYoutube size={40} />
+                  <FaYoutube size={36} />
                 </a>
               </div>
-
             </div>
           </div>
 
-          <div className="border w-full rounded-2xl bg-[#2B3D51] my-4"></div>
+          <div className="border w-full rounded-2xl bg-[#2B3D51] my-2 md:my-4"></div>
 
-          <div className="text-gray-500 mt-12 flex justify-between items-center w-full">
-            <p>© 2024 SYNC | All Rights Reserved</p>
-            <div className="flex justify-center space-x-4">
-              <a href="#" className="hover:underline">
+          <div className="text-gray-500 mt-6 md:mt-12 flex flex-col md:flex-row justify-between items-center w-full">
+            <p className="text-sm md:text-base">
+              © 2024 SYNC | All Rights Reserved
+            </p>
+            <div className="flex justify-center space-x-4 mt-4 md:mt-0">
+              <a href="#" className="hover:underline text-sm">
                 Terms of Service
               </a>
-              <a href="#" className="hover:underline">
+              <a href="#" className="hover:underline text-sm">
                 Privacy Policy
               </a>
             </div>
