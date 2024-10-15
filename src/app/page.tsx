d749613adc30b8a4bdcc3bfb0625fdd84a8482e7
Aanshuu <e21cseu0085@bennett.edu.in>
2024-10-15 14:40:47 +0530

    added slider component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,13 @@ import Landing from "./components/Landing";
 import EffortlessCarpooling from "./components/EffortlessCarpooling";
 import AboutSync from "./components/AboutSync";
 import Footer from "./components/Footer";
+import Slider from "./components/Slider";
 
 export default function Home() {
   return (
     <div>
       <Landing />
+      <Slider/>
       <EffortlessCarpooling/>
       <AboutSync/>
       <Footer/>
