09852df03ebeb124364b671881c323d2774e9fc6
Rupesh Shandilya <rupeshshandilya@Rupeshs-MacBook-Pro.local>
2024-11-06 21:26:16 +0530

    added error messgae in enum

---
+++ utils/api.error.ts
@@ -0,0 +1,10 @@
+export enum ErrorMessage {
+    NotFound = "User not Found",
+    BadRequest = "Uh-oh! Your request hit a speed bump. Give it another shot, and make sure everything’s filled in correctly. We’ll get you back on the road!",
+    Forbidden = "Access denied! It seems this route is off-limits. Check your account permissions or drop us a message if you need access.",
+    Unauthorized = "You’re not quite onboard yet! Log in to continue, or sign up to join the Sync community and start connecting with campus rides and friends.",
+    InternalServerError = "We’ve hit a technical roadblock! Our team is working to clear the way. Please hang tight and try again soon—we’ll be up and running shortly.",
+    BadGateway = "Our connection is stalled at the moment. Try refreshing, or take a quick break while we reconnect you to the best carpooling routes on campus!",
+    ServiceUnavailable = "Sync is taking a quick pit stop for maintenance. We’ll be back in a few, refreshed and ready to help you find your next ride!",
+    GatewayTimeout = "The road is a bit congested right now! Our servers are taking longer than expected. Give it a minute, or come back soon for smoother routes."
+}
