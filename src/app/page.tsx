fb4d2c9d2baac9c035076733e0c12162c18097d8
Rupesh Shandilya <rupeshshandilya@Rupeshs-MacBook-Pro.local>
2024-11-22 03:10:55 +0530

    init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ import EffortlessCarpooling from "./components/EffortlessCarpooling";
 import { useTheme } from "next-themes";
 import { useCallback, useEffect } from "react";
 import isHotkey from "is-hotkey";
-import { MacbookScrollDemo } from "./components/MacbookScroll";
+import { HeroScroll } from "./components/HeroScrollDemo";
 
 export default function Home() {
   const { setTheme } = useTheme();
@@ -37,7 +37,7 @@ export default function Home() {
     <>
       <Landing />
       <Slider />
-      <MacbookScrollDemo />
+      <HeroScroll />
       <EffortlessCarpooling />
       <AboutSync />
     </>
