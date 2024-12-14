f101076e8344fe673221e178b776297404bb581c
deevee47 <divyanshvishwakarma4@gmail.com>
2024-12-15 03:51:06 +0530

    feat: create hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,15 @@ import FAQSection from '@/components/FAQ'
 import TopFooter from '@/components/TopFooter'
 import Footer from '@/components/Footer'
 import Navbar from "@/components/Navbar";
+import Hero from '@/components/Hero';
+import Calculator from '@/components/Calculator';
 
 export default function Home() {
   return (
     <main>
       <Navbar />
+      <Hero />
+      <Calculator />
       <ToggleSection />
       <FAQSection />
       <TopFooter />
---
+++ src/components/Calculator.tsx
@@ -0,0 +1,13 @@
+import Image from 'next/image'
+import React from 'react'
+
+const Calculator = () => {
+  return (
+      <>
+          <Image className='w-[90%]' src="/assets/hero-map.png" width={8000} height={8000} alt="Hero Map" />
+     {/* TODO: Add the calculator code from previous website */}
+      </>
+  )
+}
+
+export default Calculator
