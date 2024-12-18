e56a65950d374a04d3750fe1abd6fe0e70735432
Rupesh <rupeshkshandilya@gmail.com>
2024-12-18 12:22:29 +0530

    added typewritter effect changed image to svg

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,19 +1,23 @@
+import CustomTypewriter from "@/lib/CustomTypewriter";
 import Image from "next/image";
 import React from "react";
 import { FaArrowRight } from "react-icons/fa";
 
 const Hero = () => {
+  const typeWriterData = ["Simplified"];
   return (
     <div className="bg-[url('/assets/wave.svg')] sm:pt-28 pt-20 bg-cover bg-no-repeat">
       <div className="sm:h-screen flex sm:flex-row flex-col py-10">
         <div className="left pl-4 sm:pl-10 w-full sm:w-[60%] gap-6 flex flex-col">
           <div className="text-white text-3xl sm:text-5xl font-semibold mt-10">
             Your Campus Commute,
-            <div className="text-[#099be4] mt-4">Simplified</div>
+            <div className="text-[#099be4] mt-4">
+              <CustomTypewriter typeWriterData={typeWriterData} />
+            </div>
           </div>
           <div className="text-sm md:text-xl text-[#9e9a9a] w-[85%]">
-          SuperApp designed by students for students.
-          More than just a carpooling platform – it&apos;s a vibrant community.
+            SuperApp designed by students for students. More than just a
+            carpooling platform – it&apos;s a vibrant community.
           </div>
           <div className="flex gap-6 items-center">
             <button className="px-6 py-4 h-fit max-w-xl text-white bg-[#099BE4] rounded-2xl text-lg font-medium shadow-sm shadow-white flex items-center gap-2">
@@ -31,7 +35,7 @@ const Hero = () => {
         <div className="w-full sm:w-[65%] mx-auto">
           <Image
             className="w-full sm:w-[90%] mt-16"
-            src="/assets/hero-people.png"
+            src="/assets/People.svg"
             width={900}
             height={900}
             alt="Group of People"
