7e11675f3fe81b7123611c8459d801b7e0c4979e
Rupesh <rupeshkshandilya@gmail.com>
2024-12-18 12:20:19 +0530

    changed fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,13 @@
 import Navbar from '@/components/Navbar';
 import './globals.css';
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
+import { Roboto } from 'next/font/google';
 
-const inter = Inter({ subsets: ['latin'] });
+const roboto = Roboto({
+  weight: "400",
+  style: ["italic","normal"],
+  subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
   title: "Sync",
@@ -17,7 +21,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={roboto.className}>
         <Navbar />
         {children}
       </body>
