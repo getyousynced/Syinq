2723928f8b447cb623a3a2333c67564416ed2403
Zuhaib Hanfi <hanfizuhaib@gmail.com>
2025-11-07 23:30:39 +0530

    fixed some padding issues

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 const Page = () => {
 
   return (
-    <div className='h-[100vh] w-[100vw] flex flex-col gap-10 justify-center items-center bg-gradient-to-br from-blue-200 via-white to-blue-400 '>
+    <div className='h-[100vh] w-[100vw] flex flex-col gap-10 p-5 justify-center items-center bg-gradient-to-br from-blue-200 via-white to-blue-400 '>
       <Image
         src="/logo.png"
         alt='syinq logo'
